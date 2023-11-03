8091bb63c80ed7a993d6b7c580c986cd81775a7c
hritik2002 <sharmahritik2002@gmail.com>
2023-11-04 02:14:42 +0530

    feat: add dynamic progress bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,108 +1,201 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
-import {Food} from './Food';
+import { useNavigate } from "react-router-dom";
+import { Food } from "./Food";
 
-export const Home = ()=>{
-    const  [data,setData]= useState({})
-    const [completed,setCompleted]=useState({});
-    const [userprofile,setUserprofile]=useState({})
+export const Home = () => {
+  const [data, setData] = useState({});
+  const [completed, setCompleted] = useState({});
+  const [userprofile, setUserprofile] = useState({});
 
-const [current,setCurrent]= useState("breakfast");
+  const [current, setCurrent] = useState("breakfast");
 
-    async function getUserData(){
-        try{
-            const response = await fetch("http://localhost:5000/meal-planner", {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'auth-token': localStorage.getItem('token')
-            },
-            withCredential: true
-        });
-        const json = await response.json()
-        
-        setData(prev => json)
-        setCompleted(prev=> json.completed)
-        // console.log(data)
-        }
-        catch(err){
-            console.log(err);
-        }
+  async function getUserData() {
+    try {
+      const response = await fetch("http://localhost:5000/meal-planner", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": localStorage.getItem("token"),
+        },
+        withCredential: true,
+      });
+      const json = await response.json();
+
+      setData((prev) => json);
+      setCompleted((prev) => json.completed);
+      // console.log(data)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function getUserProfile() {
+    try {
+      const response = await fetch("http://localhost:5000/profile", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": localStorage.getItem("token"),
+        },
+        withCredential: true,
+      });
+      const json = await response.json();
+      setUserprofile(json);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getUserData();
+    getUserProfile();
+    const val = getMealBasedOnTime();
+    setCurrent((prev) => val);
+  }, []);
+
+  const navigate = useNavigate();
+
+  function getMealBasedOnTime() {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+
+    if (hours >= 6 && hours < 12) {
+      return "breakfast";
+    } else if (hours >= 12 && hours < 18) {
+      return "lunch";
+    } else {
+      return "dinner";
+    }
+  }
+
+  //    console.log(userprofile);
+
+  const getPercentage = (numerator, totalCalorie) => {
+    if(!numerator || !totalCalorie) {
+        return 0;
     }
 
-    async function getUserProfile (){
-        try{
-            const response=await fetch('http://localhost:5000/profile',{
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'auth-token': localStorage.getItem('token')
-                },
-                withCredential: true
-            });
-            const json =await response.json()
-            setUserprofile(json);
-        }
-        catch(err){
-            console.log(err);
-        }
-    }
-    
-    useEffect(()=>{
-        getUserData();
-        getUserProfile();
-        const val=getMealBasedOnTime();
-        setCurrent(prev => val)
-    },[])
+    return numerator * 1.0 / totalCalorie;
+  }
 
-    const navigate = useNavigate();
-
-    function getMealBasedOnTime() {
-        const currentTime = new Date();
-        const hours = currentTime.getHours();
-
-        if (hours >= 6 && hours < 12) {
-            return "breakfast";
-        } else if (hours >= 12 && hours < 18) {
-            return "lunch";
-        } else {
-            return "dinner";
-        }
-    }
-
-
-//    console.log(userprofile);
-   
-    return(
-        <div className="home">
-            <div className="home1">
-            <h4>Welcome, {userprofile?.name}!</h4> <img src='images/image 1.png'></img>
-            </div>
-            <div className="home2">
-                <div className="home2-macros">
-                    <img src="images/Group 4.png"></img>
-                    <ul className="list-macros">
-                        <l1><span className="macro-name">Carbs:</span>{data?.macros_consumed?.carbs}/{data?.meals?.breakfast?.macro_goals?.carbs + data?.meals?.dinner?.macro_goals?.carbs + data?.meals?.lunch?.macro_goals?.carbs} g</l1>
-                        <l1><span className="macro-name">Fats:</span>{data?.macros_consumed?.fats}/{data?.meals?.breakfast?.macro_goals?.fats + data?.meals?.dinner?.macro_goals?.fats + data?.meals?.lunch?.macro_goals?.fats}g</l1>
-                        <l1><span className="macro-name">Proteins:</span>{data?.macros_consumed?.protein}/{data?.meals?.breakfast?.macro_goals?.protein + data?.meals?.dinner?.macro_goals?.protein + data?.meals?.lunch?.macro_goals?.protein}g</l1>
-                    </ul>
-                </div>
-                <div className="MACROS">MACROS</div>
-                <div className="goal-home">
-                    <img src='images/image 2.png'></img>
-                    <span>{userprofile?.current_goal}</span>
-                </div>
-                {/* <img src="images/Group 5.png"></img> */}
-                <img src='images/Group 3 (1).png'></img>
-            </div>
-
-        <div className="home3">
-            
-            {current==="breakfast" && <Food mealData={data?.meals?.breakfast} current={current} completed={completed} setCompleted={setCompleted} data={data} setData={setData}/>}
-            {current==="lunch" && <Food mealData={data?.meals?.lunch} current={current} completed={completed} setCompleted={setCompleted} data={data} setData={setData}/>}
-            {current==="dinner" && <Food mealData={data?.meals?.dinner} current={current} completed={completed} setCompleted={setCompleted} data={data} setData={setData} />}
+  return (
+    <div className="home">
+      <div className="home1">
+        <h4>Welcome, {userprofile?.name}!</h4>{" "}
+        <img src="images/image 1.png"></img>
+      </div>
+      <div className="home2">
+        <div className="home2-macros">
+          <img src="images/Group 4.png"></img>
+          <svg viewBox="0 0 100 100">
+            <circle
+              stroke="green"
+              stroke-width="5.5"
+              cx="50"
+              cy="50"
+              r="30"
+              fill="none"
+            />
+            <circle
+              strokeDashoffset={getPercentage(data?.macros_consumed?.calories, data?.meals?.breakfast?.macro_goals?.carbs + data?.meals?.dinner?.macro_goals?.carbs + data?.meals?.lunch?.macro_goals?.carbs)}
+              id="progress--circle"
+              stroke="#28411B"
+              stroke-width="5.5"
+              cx="50"
+              cy="50"
+              r="30"
+              fill="none"
+              pathLength="100"
+            />
+            <text
+              id="progress--text"
+              x="50"
+              y="48"
+              text-anchor="middle"
+              dominant-baseline="middle"
+            >
+              {data?.macros_consumed?.calories} / {data?.meals?.breakfast?.macro_goals?.carbs + data?.meals?.dinner?.macro_goals?.carbs + data?.meals?.lunch?.macro_goals?.carbs}
+            </text>
+            <text
+              id="progress--text"
+              class="progress--text--second"
+              x="50"
+              y="55"
+              text-anchor="middle"
+              dominant-baseline="middle"
+            >
+              Calories (kcal)
+            </text>
+          </svg>
+          <ul className="list-macros">
+            <l1>
+              <span className="macro-name">Carbs:</span>
+              {data?.macros_consumed?.carbs}/
+              {data?.meals?.breakfast?.macro_goals?.carbs +
+                data?.meals?.dinner?.macro_goals?.carbs +
+                data?.meals?.lunch?.macro_goals?.carbs}{" "}
+              g
+            </l1>
+            <l1>
+              <span className="macro-name">Fats:</span>
+              {data?.macros_consumed?.fats}/
+              {data?.meals?.breakfast?.macro_goals?.fats +
+                data?.meals?.dinner?.macro_goals?.fats +
+                data?.meals?.lunch?.macro_goals?.fats}
+              g
+            </l1>
+            <l1>
+              <span className="macro-name">Proteins:</span>
+              {data?.macros_consumed?.protein}/
+              {data?.meals?.breakfast?.macro_goals?.protein +
+                data?.meals?.dinner?.macro_goals?.protein +
+                data?.meals?.lunch?.macro_goals?.protein}
+              g
+            </l1>
+          </ul>
         </div>
+        <div className="MACROS">MACROS</div>
+        <div className="goal-home">
+          <img src="images/image 2.png"></img>
+          <span>{userprofile?.current_goal}</span>
         </div>
-    )
-}
+        {/* <img src="images/Group 5.png"></img> */}
+        <img src="images/Group 3 (1).png"></img>
+      </div>
+
+      <div className="home3">
+        {current === "breakfast" && (
+          <Food
+            mealData={data?.meals?.breakfast}
+            current={current}
+            completed={completed}
+            setCompleted={setCompleted}
+            data={data}
+            setData={setData}
+          />
+        )}
+        {current === "lunch" && (
+          <Food
+            mealData={data?.meals?.lunch}
+            current={current}
+            completed={completed}
+            setCompleted={setCompleted}
+            data={data}
+            setData={setData}
+          />
+        )}
+        {current === "dinner" && (
+          <Food
+            mealData={data?.meals?.dinner}
+            current={current}
+            completed={completed}
+            setCompleted={setCompleted}
+            data={data}
+            setData={setData}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
